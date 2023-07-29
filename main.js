bc784c5be8af67da20822509b17e98d4cab828fd
matin <matinhoseinzadeh@yahoo.com>
2023-07-29 19:56:11 +0330

    exercise week one and week two

--- main.js
+++ main.js
@@ -1,19 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
   var app = new Vue({
-    el: '#Exercise',
+    el: "#app",
     data: {
-      description: {
-        value: 'A pair of warm, fuzzy socks',
+      brand: "Vue Mastery",
+      product: "Socks",
+      description: "A pair of warm, fuzzy socks",
+      selectedVariant: 0,
+      altText: "A pair of Socks",
+      link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [{
+          variantId: 2234,
+          variantColor: "green",
+          variantImg: "./assets/socks-green.jpg",
+          variantQuantity: 1,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImg: "./assets/socks-blue.jpg",
+          variantQuantity: 0,
+        },
+      ],
+      onSale: true,
+      sizes: ["Small", "Medium", "Large", "XLarge"],
+      cart: 0,
+    },
+    methods: {
+      addToCart() {
+        this.cart += 1;
       },
-      Link: {
-        href: 'https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding'
-
+      removeFromCart() {
+        if (this.cart > 0) {
+          this.cart -= 1;
+        }
       },
-      Title: {
-        text: 'This is a Title of Link'
+      updateProduct(index) {
+        this.selectedVariant = index;
+        console.log(index);
       },
-      inSale: true
-    }
-  })
+    },
+    computed: {
+      title() {
+        return `${this.brand} ${this.product}`;
+      },
+      imageSrc() {
+        return this.variants[this.selectedVariant].variantImg;
+      },
+      inStock() {
+        return this.variants[this.selectedVariant].variantQuantity;
+      },
+      sale() {
+        if (this.onSale) {
+          return `${this.brand} ${this.product} are on sale!`;
+        }
+        return `${this.brand} ${this.product} are not on sale!`;
+      },
+    },
+  });
   Vue.config.devtools = true;
 });
