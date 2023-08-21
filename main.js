c304c8115472ce8c20497ca63a7849402abdd3c2
matin <matinhoseinzadeh@yahoo.com>
2023-08-21 18:27:15 +0330

    do exercises for FORM and TAB

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+    var eventBus = new Vue()
+
 Vue.component("moreDetail", {
   props: {
     details: {
@@ -12,6 +14,49 @@ Vue.component("moreDetail", {
   </ul>
   `,
 })
+
+Vue.component('product-tabs', {
+  props: {
+      reviews: {
+        type: Array,
+        required: false
+      }
+    },
+  template: `
+    <div class="tab_box">
+      <span class="tab" :class="{ activeTab: selectedTab === tab }"  v-for="(tab, index) in tabs" :key="index"
+      @click="selectedTab = tab">{{ tab }}</span>
+
+      <div v-show="selectedTab === 'Reviews'">
+      <h2>Reviews</h2>
+      <p v-if="!reviews.length">
+        There are no reviews yet.
+      </p>
+      <ul>
+        <li v-for="review in reviews">
+          <p>
+            {{ review.name }}
+          </p>
+          <p>
+            Rating: {{ review.rating }}
+          </p>
+          <p>
+            {{ review.review }}
+          </p>
+        </li>
+      </ul>
+    </div>
+    <product-review v-show="selectedTab === 'Make a Review'" ></product-review>
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ['Reviews', 'Make a Review']      ,
+      selectedTab: 'Reviews' ,
+    }
+  }
+})
+
 Vue.component("product", {
   props: {
     premium: {
@@ -51,7 +96,9 @@ Vue.component("product", {
         Add to cart
       </button>
       <button @click="removeFromCart()">Remove from cart</button>
+       <product-tabs :reviews="reviews"></product-tabs>
     </div>
+    
   </div>
   `,
   data() {
@@ -78,6 +125,7 @@ Vue.component("product", {
       ],
       onSale: true,
       sizes: ["Small", "Medium", "Large", "XLarge"],
+      reviews: [],
     }
   },
   methods: {
@@ -92,7 +140,7 @@ Vue.component("product", {
     },
     updateProduct(index) {
       this.selectedVariant = index;
-    },
+    }
   },
   computed: {
     title() {
@@ -115,6 +163,106 @@ Vue.component("product", {
         return "Free";
       }
       return 2.99;
+    },
+  },
+      mounted() {
+      eventBus.$on('review-submitted', productReview => {
+        this.reviews.push(productReview)
+      })
+    }
+});
+Vue.component("product-review", {
+  template: `
+    <form class="review-form" @submit.prevent="onSubmit">
+      <p v-if="errors.length">
+        <b>
+          Please correct the following error(s) 
+        </b>
+        <ul>
+          <li v-for="error in errors">
+            {{ error }}
+          </li>
+        </ul>
+      </p>
+      <p>
+        <label for="name">
+          Name:
+        </label>
+        <input id="name" v-model="name">
+      </p>
+      <p>
+        <label for="review">
+          Review:
+        </label>
+        <textarea id="review" v-model="review"></textarea>
+      </p>
+      <p>
+        <label for="rating">
+          Rating:
+        </label>
+        <select id="rating" v-model.number="rating">
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+      </p>
+      <p>
+        Would you recommend this product?
+      </p>
+      <label>
+        Yes
+        <input type="radio" value="Yes" v-model="recommend">
+      </label>
+      <br>
+      <label>
+        No
+        <input type="radio" value="No" v-model="recommend">
+      </label>
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
+    <input v-model="name">
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: [],
+      recommend: false,
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.rating && this.review && this.recommend) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+          recommend: this.recommend,
+        };
+        eventBus.$emit("review-submitted", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+        this.recommend = null;
+      } else {
+        if (!this.name) {
+          this.errors.push("Name required");
+        }
+        if (!this.rating) {
+          this.errors.push("Rating required");
+        }
+        if (!this.review) {
+          this.errors.push("Review required");
+        }
+        if (!this.recommend) {
+          this.errors.push("Recommend required");
+        }
+      }
     },
   },
 });
